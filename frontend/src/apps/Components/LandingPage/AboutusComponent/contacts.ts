9269c51f3eca55fb35c3defbf917e115fab1f47c
elayneh <betenatefera@gmail.com>
2024-05-19 22:14:48 +0300

    Description for the home page is added

--- frontend/src/apps/Components/LandingPage/AboutusComponent/contacts.ts
+++ frontend/src/apps/Components/LandingPage/AboutusComponent/contacts.ts
@@ -2,7 +2,7 @@ export const AboutUs = [
   {
     title: "Discover Ethiopia: A Land of Wonders",
     body: "Welcome to Ethiopia, the land of ancient history, stunning landscapes, and vibrant culture. Explore majestic historical sites like the rock-hewn churches of Lalibela, the ancient city of Aksum, and the dramatic Simien Mountains. Immerse yourself in the rich traditions and warm hospitality of the Ethiopian people. Experience the beauty and mystery of Ethiopia through our cutting-edge VR/AR technology, and get detailed insights with OpenAI-powered explanations. Embark on an unforgettable journey today!",
-  },
+  },  
   {
     title: "Real-Time Communication",
     body: "Seamlessly connect job seekers and recruiters through our integrated chat application. Experience real-time messaging, file sharing, interview scheduling, and automated reminders. Enhance communication efficiency and engagement, fostering a dynamic and interactive connection between both parties.",
