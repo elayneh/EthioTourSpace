1a88af6046661ecca1e86f2f6ba53c85db4d729c
natnaelLake <bewuketulake1212@gmail.com>
2024-05-19 10:00:14 +0300

    embed the 3D model design

--- frontend/src/apps/Components/3DDisplayComponent/constants.ts
+++ frontend/src/apps/Components/3DDisplayComponent/constants.ts
@@ -15,47 +15,55 @@ export const gridData = [
     id: "1",
     title: "Lalibela, Bete Giorgis",
     description: "High-end smartphone with great camera",
+    link:"https://sketchfab.com/models/bbd290c2a74e46af9f0671573251034d/embed"
   },
   {
     id: "2",
     title: "Galaxy Ultra S22",
     price: 78,
     description: "Latest Samsung flagship with advanced features",
+    link:"https://sketchfab.com/models/bbd290c2a74e46af9f0671573251034d/embed"
   },
   {
     id: "3",
     title: "iPhone 16 Pro",
     price: 569,
     description: "New generation iPhone with powerful performance",
+    link:"https://sketchfab.com/models/bbd290c2a74e46af9f0671573251034d/embed"
   },
   {
     id: "4",
     title: "Google Pixel 6 Pro",
     price: 456,
     description: "Google's latest smartphone with AI capabilities",
+    link:"https://sketchfab.com/models/bbd290c2a74e46af9f0671573251034d/embed"
   },
   {
     id: "5",
     title: "Xiaomi 12",
     price: 778,
     description: "Affordable flagship with excellent specs",
+    link:"https://sketchfab.com/models/bbd290c2a74e46af9f0671573251034d/embed"
   },
   {
     id: "6",
     title: "Dell Latitude",
     price: 547,
     description: "Business laptop with reliable performance",
+    link:"https://sketchfab.com/models/bbd290c2a74e46af9f0671573251034d/embed"
   },
   {
     id: "7",
     title: "iPhone 15 Plus",
     price: 9099,
     description: "Large screen iPhone with advanced features",
+    link:"https://sketchfab.com/models/bbd290c2a74e46af9f0671573251034d/embed"
   },
   {
     id: "8",
     title: "iPad Mini",
     price: 454,
     description: "Compact iPad with powerful features",
+    link:"https://sketchfab.com/models/bbd290c2a74e46af9f0671573251034d/embed"
   },
 ];
